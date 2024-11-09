aca5c1f732d19a5b99c4f8c86ab6c4340e5b6735
ShakthiSundar-K <shakthisundar01@gmail.com>
2024-11-09 14:07:05 +0530

    Cors Policy update

--- backend/index.js
+++ backend/index.js
@@ -5,15 +5,15 @@ import AppRoutes from "./src/routes/index.js";
 
 const app = express();
 
-const corsOptions = {
-  origin:
-    "https://online-personalized-fitness-class-booking-platform-fit-pro.vercel.app", // Remove trailing slash
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // Allow cookies to be sent with cross-origin requests
-};
+// const corsOptions = {
+//   origin:
+//     "https://online-personalized-fitness-class-booking-platform-fit-pro.vercel.app", // Remove trailing slash
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   //   credentials: true, // Allow cookies to be sent with cross-origin requests
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 
 app.use(AppRoutes);
